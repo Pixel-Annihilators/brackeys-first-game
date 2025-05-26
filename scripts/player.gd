fcf7fd97034baec519a04063160acf964ece795c
Shoban Chiddarth <messageshoban@gmail.com>
2025-05-26 16:04:46 +0530

    Rebinded inputs
    - Jump: W , Space
    - Left: A, <-
    - Right: D, ->

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,11 +12,11 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
