dba4c6b44c7f1ba893124f2a1855fcb1786c36c3
Shoban Chiddarth <messageshoban@gmail.com>
2025-05-27 10:36:51 +0530

    Made the player look in the direction it is moving

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -15,8 +17,16 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# no move: 0
+	# left: -1
+	# right: +1
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction==1:
+		animated_sprite.flip_h = false
+	elif direction==-1:
+		animated_sprite.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
